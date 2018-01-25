5a6d01ebaca420cc5f7ad462a25d045fd75de09a
UsER <UsER@AFA>
2018-01-25 11:38:56 +0300

    dd

--- proje1/src/proje1/main.java
+++ proje1/src/proje1/main.java
@@ -4,5 +4,7 @@ public class main {
 public static void main(String... args) {
 	System.out.println("2a");
 	System.out.println("aaa");
+	System.out.println("bbb");
+	System.out.println("bbb");
 }
 }
